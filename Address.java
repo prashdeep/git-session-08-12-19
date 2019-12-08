ea0982f19cb2dc10a600dac3577d53a7938f6ba2
prashdeep <pradeep.kumar44@gmail.com>
2019-12-08 15:20:24 +0530

    committed the Address class

---
+++ Address.java
@@ -0,0 +1,25 @@
+public class Address {
+    private int addressId;
+    private String city;
+    private String zipCode;
+    
+	public int getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+    
+}
